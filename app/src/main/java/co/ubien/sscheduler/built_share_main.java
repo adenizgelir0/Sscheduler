62e86315fe5b32e08d7b01b260ebca6441cba1eb
Mehmet Efe Sak <mefesak007@gmail.com>
2023-05-16 16:13:41 +0300

    BuiltShareFirstImplementatin
    İbrahim kalp

---
+++ app/src/main/java/co/ubien/sscheduler/built_share_main.java
@@ -0,0 +1,14 @@
+package co.ubien.sscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class built_share_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_built_share_main);
+    }
+}
