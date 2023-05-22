308e21dc4ff2770585f5c149dad5a5a757aa151b
adenizgelir0 <adenizgelir@protonmail.com>
2023-05-22 23:59:50 +0300

    Event classında değişiklik

--- app/src/main/java/co/ubien/sscheduler/Event.java
+++ app/src/main/java/co/ubien/sscheduler/Event.java
@@ -6,14 +6,12 @@ public class Event {
     private int end;
     private String name;
     private int color;
-    private int day;
 
     public Event(int start, int end, String name, int color, int day) {
-        this.start = start;
-        this.end = end;
+        this.start = start + day*24*60;
+        this.end = end + day*24*60;
         this.name = name;
         this.color = color;
-        this.day = day;
     }
     public Event(int start, int end, String name, int color) {
         this.start = start;
@@ -32,16 +30,35 @@ public class Event {
     public int getStart() {
         return start;
     }
+    public int getStartMins(){
+        return start % (24*60);
+    }
+    public int getEndMins(){
+        return end % (24*60);
+    }
 
     public int getEnd() {
         return end;
     }
 
     public int getDay() {
-        return day;
+        return start / (24*60);
     }
 
     public void setDay(int day) {
-        this.day = day;
+        this.start %= 24*60;
+        this.start += 24*60*day;
+        this.end %= 24*60;
+        this.end += 24*60*day;
+    }
+
+    public boolean intersects(Event E)
+    {
+        /*
+          start1 <= C < end1
+          start2 <= C < end2
+        */
+       return this.start < E.getEnd()
+            && E.getStart() < this.end;
     }
 }
--- app/src/main/java/co/ubien/sscheduler/Schedule.java
+++ app/src/main/java/co/ubien/sscheduler/Schedule.java
@@ -5,11 +5,26 @@ import java.util.ArrayList;
 public class Schedule {
     private ArrayList<Event> events;
 
+    public Schedule(ArrayList<Event> events)
+    {
+        this.events = events;
+    }
     public Schedule() {
         events = new ArrayList<>();
     }
     public ArrayList<Event> getEvents(){
         return events;
+    }
+    public void addEvent(Event E)
+    {
+       // Event copy = new Event()
+        boolean notFound = true;
+        while(notFound)
+        {
+            for(Event e : events){
+              //  if(Event)
+            }
+        }
     }
 
 }
