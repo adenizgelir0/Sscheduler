4b0c11389c21d96c668219058b74f9bd08b49e86
Mehmet Efe Sak <mefesak007@gmail.com>
2023-05-25 10:04:20 +0300

    Post Class Created

---
+++ app/src/main/java/co/ubien/sscheduler/Post.java
@@ -0,0 +1,10 @@
+package co.ubien.sscheduler;
+
+public class Post {
+    public int like;
+    public int disLike;
+    public User user;
+    public Schedule schedule;
+
+    //public Post(Schedule schedule);
+}
