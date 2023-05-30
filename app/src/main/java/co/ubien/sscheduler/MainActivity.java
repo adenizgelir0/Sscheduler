979ab6bc88dfb3b683ed4fd413a4f8da4697a1d3
İbrahim Karaca <ayaz.karaca@yahoo.com>
2023-05-30 19:25:03 +0300

    app toolbar v1

--- app/src/main/java/co/ubien/sscheduler/MainActivity.java
+++ app/src/main/java/co/ubien/sscheduler/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //auth.signOut();
         FirebaseUser user = auth.getCurrentUser();
         //Log.i("MainActivity",user.getUid());
         if(user != null){
